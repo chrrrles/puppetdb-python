c860fcc442c7664be0a93d2cf25a91e00697dff6
monk-ee <magic.monkee.magic@gmail.com>
2014-05-14 15:51:53 +1000

    change license and setup

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.0.1'
+version = '0.0.2'
 setup(
     name='puppetdb',
     version=version,
     description='Python client for interacting with PuppetDB',
-    url='http://github.com/arcus-io/puppetdb-python',
-    download_url=('https://github.com/arcus-io/'
+    url='http://github.com/monk-ee/puppetdb-python',
+    download_url=('https://github.com/monk-ee/'
                   'puppetdb-python/archive/%s.tar.gz' % version),
-    author='Arcus, Inc.',
-    author_email='support@arcus.io',
+    author='monk-ee',
+    author_email='magic.monkee.magic@gmail.com',
     keywords=['puppet', 'puppetdb'],
     license='Apache 2.0',
     packages=['puppetdb','puppetdb/v2'],
@@ -22,7 +22,7 @@ setup(
     test_suite='tests.all_tests',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: GNU GPL v3.0',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
